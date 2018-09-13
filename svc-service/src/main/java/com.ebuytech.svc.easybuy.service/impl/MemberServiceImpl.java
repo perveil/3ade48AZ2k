ac74609a742483a2585a5c47c5922f64fdbfc153
airmacx <1356348551@qq.com>
2018-09-13 19:50:19 +0800

    3rd exhandler

--- svc-service/src/main/java/com.ebuytech.svc.easybuy.service/impl/MemberServiceImpl.java
+++ svc-service/src/main/java/com.ebuytech.svc.easybuy.service/impl/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package com.ebuytech.svc.easybuy.service.impl;
 
 import com.ebuytech.svc.easybuy.dao.MemberDAO;
 import com.ebuytech.svc.easybuy.entity.Member;
+import com.ebuytech.svc.easybuy.enums.ResultEnums;
+import com.ebuytech.svc.easybuy.exception.ClientException;
 import com.ebuytech.svc.easybuy.service.IMemberService;
 import com.ebuytech.svc.easybuy.util.RedisUtil;
 import com.ebuytech.svc.easybuy.util.UUIDUtils;
@@ -23,15 +25,16 @@ public class MemberServiceImpl implements IMemberService {
 
     @Override
     public boolean addMember(String openId, String memberName, String sex, String telephone) {
-        Member member = new Member();
-        //自动生成的mybatis mapper 插入操作里没有带主键，需手动修改
-        member.setMemberId(UUIDUtils.getUUID());
-        System.out.println(member.getMemberId());
-        member.setMemberName(memberName);
-        member.setSex(sex);
-        member.setTelephone(telephone);
-        redisUtil.set("1","23");
-        System.out.println(redisUtil.get("1"));
-        return memberDAO.insertSelective(member) > 0;
+        throw new ClientException(ResultEnums.TEST_FAIL);
+//        Member member = new Member();
+//        //自动生成的mybatis mapper 插入操作里没有带主键，需手动修改
+//        member.setMemberId(UUIDUtils.getUUID());
+//        System.out.println(member.getMemberId());
+//        member.setMemberName(memberName);
+//        member.setSex(sex);
+//        member.setTelephone(telephone);
+//        redisUtil.set("1","23");
+//        System.out.println(redisUtil.get("1"));
+//        return memberDAO.insertSelective(member) > 0;
     }
 }
---
+++ svc-web/src/main/java/com/ebuytech/svc/easybuy/web/handle/ExceptionHandlers.java
@@ -0,0 +1,45 @@
+package com.ebuytech.svc.easybuy.web.handle;
+
+import com.ebuytech.svc.easybuy.exception.AdminException;
+import com.ebuytech.svc.easybuy.exception.ClientException;
+import com.ebuytech.svc.easybuy.util.Res;
+import com.ebuytech.svc.easybuy.util.ResUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Component
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlers {
+
+    @ExceptionHandler(value = AdminException.class)
+    @ResponseBody
+    public Res lockersHandle(Exception e){
+        if(e instanceof AdminException){
+            AdminException lockersException=(AdminException) e;
+            log.error(String.valueOf(lockersException.getCode()) + "   " + String.valueOf(lockersException.getMessage()));
+            return ResUtil.error(lockersException.getCode(),lockersException.getMessage());
+        }else {
+            log.error("【系统异常】：{}",e);
+            return ResUtil.error(-1,"未知错误");
+        }
+
+    }
+
+    @ExceptionHandler(value = ClientException.class)
+    @ResponseBody
+    public Res socketHandle(Exception e){
+        if(e instanceof ClientException){
+            ClientException socketException=(ClientException) e;
+            log.error("【小程序异常】：{}",e);
+            return ResUtil.error(socketException.getCode(),socketException.getMessage());
+        }else {
+            log.error("【小程序异常】：{}",e);
+            return ResUtil.error(-1,"未知错误");
+        }
+
+    }
+}
